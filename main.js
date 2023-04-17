35ef8de9725962398783b3d005cd10ee101c7692
amirhossein <hoseinia0870@gmail.com>
2023-04-17 18:52:07 +0430

    khalij update

---
+++ main.js
@@ -0,0 +1,48 @@
+
+function disableMouseScroll() {
+    return false;
+}
+const navshow = () =>{
+    document.getElementById("nav-list").classList.add("show")
+    document.getElementById("nav-overlay").classList.add("active")
+    document.body.style = "visibility:visable;height:100vh;width:100vw;overflow:hidden"
+}
+const navclose = () =>{
+    document.getElementById("nav-list").classList.remove("show")
+    document.getElementById("nav-overlay").classList.remove("active")
+    document.body.style = "visibility:visable;height:initial;width:100%;overflow-x:hidden"
+}
+const menuitem = (name) =>{
+    document.getElementById("menu-item-wrapper").innerHTML = name;
+    document.getElementById("menu-item-wrapper").style.color = "#66fcf1"
+}
+const menuitemleave = () =>{
+    document.getElementById("menu-item-wrapper").style.color = "#0b0c10"
+    document.getElementById("menu-item-wrapper").innerHTML = "aaaa"
+}
+const servicesshow = () =>{
+    document.getElementById("about-item-show").classList.add("showservices");
+}
+const servicesclose = () =>{
+    document.getElementById("about-item-show").classList.remove("showservices");
+}
+const productadd = () =>{
+    const oldvalue = parseInt(document.getElementById("product-number").innerHTML);
+    const newvalue = oldvalue + 1;
+    document.getElementById("product-number").innerHTML = newvalue; 
+    document.getElementById("counterinput").value = newvalue;
+    console.log(document.getElementById("counterinput").value)
+}
+const productremove = () =>{
+    const oldvalue = parseInt(document.getElementById("product-number").innerHTML);
+    const newvalue = oldvalue - 1;
+    if (newvalue < 0){
+        newvalue = oldvalue;
+    }
+    document.getElementById("product-number").innerHTML = newvalue; 
+    document.getElementById("counterinput").value = newvalue;
+    console.log(document.getElementById("counterinput").value)
+}
+const smallimage = (small) =>{
+    document.getElementById("big-image").src = small.src;
+}
